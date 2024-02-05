ea0dd70bdd0fa82f8a4242d7217b31704427f6d3
whereasjovially@outlook.com <whereasjovially@outlook.com>
2024-02-05 10:55:39 +0900

    changed: keeper module

--- testutil/keeper/deal.go
+++ testutil/keeper/deal.go
@@ -2,7 +2,6 @@ package keeper
 
 import (
 	"testing"
-
 	tmdb "github.com/cometbft/cometbft-db"
 	"github.com/cometbft/cometbft/libs/log"
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
